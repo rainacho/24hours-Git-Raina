df61754b03b137fe740f88b8ae510705d61a9831
Raina Cho <chohr82@gmail.com>
2018-05-12 17:36:14 -0400

    Delete database.php
    Delete database.php from the Git-Hub

--- includes/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $dsn = "mysql:host=localhost;dbname=24hours";
-
-    $db_username = "rainacho";
-    $db_password = "3@r87n*SN3OR";
-
-    $pdo = new PDO($dsn, $db_username, $db_password);
-
-?>
